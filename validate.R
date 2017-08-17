0285db90ecfb95debc9c5f496404587db32ffbaa
Alex O. Holcombe <aoholcombe@gmail.com>
2017-08-18 07:08:48 +1000

    what I Said last time

---
+++ validate.R
@@ -0,0 +1,9 @@
+setwd("/Users/alexh/Documents/attention_tempresltn/EEGRSVP/EEG_RSVP")
+
+#random orientation each trial. First experiment of second backwards-letters paper
+
+mixtureModelOutputPath<-"/Users/alexh/Google\ Drive/Backwards\ paper/secondPaper/E1/Data/MixModData"
+importedToRbyChris <- "allParams.RData"
+
+#raw data path containing .mat file for each subject
+rawDataPath<- "/Users/alexh/Google\ Drive/Backwards\ paper/secondPaper/E1/Data/RawData/Data/"
