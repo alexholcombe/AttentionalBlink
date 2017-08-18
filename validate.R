04087c59061fa226e5c599a28eff621388279811
Alex O. Holcombe <aoholcombe@gmail.com>
2017-08-19 06:36:13 +1000

    it worked? look into warnings

--- validate.R
+++ validate.R
@@ -16,38 +16,41 @@ data<- readRDS("data/alexImportBackwardsPaper2E1.Rdata") #.mat file been preproc
 
 condtnVariableNames <- c("expDegrees")
 
-# Work out possible positions in the stream for T1.
-#Is the values in allTargets the positions of the targets in the stream?
-# Indeed, looks like allTargets[1,1] is position of one target in first trial, allTargets[1,2] is position of other target
-str(data$allTargets)
-unique(data$allTargets)
-target1Pos<- data$allTargets[,1] #target1 position each trial
-
-possibleT1Pos <- sort(unique(target1Pos))
-
-numItemsInStream <- length( data$allLetterOrder[1,1,] )
-#possiblePos <- sort( unique(data$allResponses[,1]) ) #serial position of response1 every trial
-
-nT1Pos <- length(listT1Pos)
-
-
+directFromMAT <- FALSE
+if (directFromMAT) {
+  # Work out possible positions in the stream for T1.
+  #Is the values in allTargets the positions of the targets in the stream?
+  # Indeed, looks like allTargets[1,1] is position of one target in first trial, allTargets[1,2] is position of other target
+  targetSP<- data$allTargets[,1] #target1 position each trial
+  possibleTargetSP <- sort(unique(targetSP))
+  numItemsInStream <- length( data$allLetterOrder[1,1,] )
+  #possiblePos <- sort( unique(data$allResponses[,1]) ) #serial position of response1 every trial
+  nTargetPos <- length(targetSP)
+} else {
+  possibleTargetSP<- sort(unique(data$targetSP))
+}
 # Calculate the domain of possible errors (xDomain).
-minT1pos <- min(possibleT1Pos)
-maxT1pos <- max(possibleT1Pos)
-minSPE <- 1 - maxT1pos #-1
-maxSPE <- numItemsInStream - minT1pos #+1
+minTargetSP <- min(possibleTargetSP)
+maxTargetSP <- max(possibleTargetSP)
+minSPE <- 1 - maxTargetSP
+maxSPE <- numItemsInStream - minTargetSP
 xDomain<- minSPE:maxSPE
+
+#Would be nice to add my helper function to take note of whether counterbalanced
   
 # Generate the 'pseudo-uniform' distribution, which is the expected distribution of errors if a random guess was
 # provided on every trial. This isn't an actual uniform distribution because the most extreme errors are only
 # possible on trials in which targets appear at their most extreme positions.
 pseudoUniform <- matrix(xDomain, ncol=2,nrow=35, byrow=FALSE)
+#first column will be SPE. Second column will be expected frequency of that SPE from guessing
 pseudoUniform[,2] <-0
 # Cycle through each possible T1 position.
-for (i in 1:length( target1Pos )) {  #empirical T1 positions (doesn't assume was perfectly counterbalanced)
+# Will use empirical T1 positions (doesn't assume was perfectly counterbalanced), because that yields the objective expectation
+# if you simply guess on every trial.
+for (i in 1:length( data$targetSP )) {  
 #for (i in 1:length(possibleT1Pos)){ #theoretical  (assumes each possible position occurred equally often)
 
-  thisPos <- target1Pos[i]   #For example, the first position number might be the 7th position in the stream.
+  thisPos <- data$targetSP[i]   #For example, the first position number might be the 7th position in the stream.
   
   # Add to the pseudo-uniform distribution one unit for every
   # possible error given that T1 position.
@@ -64,8 +67,8 @@ for (i in 1:length( target1Pos )) {  #empirical T1 positions (doesn't assume was
   maxThis <- numItemsInStream-thisPos-minSPE+1
   pseudoUniform[minThis:maxThis,2] = pseudoUniform[minThis:maxThis,2] + 1
 }
-#Have to pad with extra zero
-
+#Have to pad with extra zero for some reason to make fit work
+pseudo_uniform<- c(0, pseudoUniform[,2], 0)
 
 # Run the MLE function.
 # [currentEstimates, currentCIs] <- mle(theseT1Error, 'pdf', pdf_normmixture_single, 'start', parameterGuess, 'lower', parameterLowerBound, 'upper', parameterUpperBound, 'options', options)
@@ -86,6 +89,29 @@ fitModel <- function(SPEs, parameterGuess)
   return(fit$par)                    
 }
 
-SPEs<- data$allResponses[,1] - data$allTargets[
+# Set some model-fitting parameters.
+nReplicates <- 100# Number of times to repeat each fit with different starting values
+smallNonZeroNumber <- 10^-5# Useful number for when limits can't be exactly zero but can be anything larger
+fitMaxIter <- 10^4# Maximum number of fit iterations
+fitMaxFunEvals <- 10^4# Maximum number of model evaluations
 
-fitModel(
+# Randomise starting values for each parameter.
+# Set some parameter bounds. Pat apparently found these were needed to 
+# prevent over-fitting to blips in the distributions. These
+# values are about right in most cases, but might need some tweaking if
+# e.g. you were analysing data with an unusually high or low item rate.
+muBound <- 4   #will only consider -4 to +4 for mu 
+sigmaBound <- 4 #will only consider 0 to 4 for sigma
+
+pGuess <- max(c(smallNonZeroNumber, runif(1)))
+muGuess <- (2*muBound*runif(1))-muBound
+sigmaGuess <- sigmaBound*runif(1)+smallNonZeroNumber
+
+# Compile to feed into the MLE function.
+parameterGuess <- c(pGuess, muGuess, sigmaGuess)
+
+cat("parameterGuess", parameterGuess, "\n")
+params <- fitModel(data$SPE, parameterGuess)
+cat("this estimate params=", params, "\n")
+
+
